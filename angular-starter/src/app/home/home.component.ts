b87f5e5221eed96efd854a50af35d1cfc70195ba
Derek Johnson <DerekJ@ixfactor.com>
2019-06-11 17:21:11 -0500

    Fix Security Alerts

--- angular-starter/src/app/home/home.component.ts
+++ angular-starter/src/app/home/home.component.ts
@@ -11,16 +11,17 @@ export class HomeComponent implements OnInit, AfterViewInit {
   constructor(public tourService: TourService) { }
 
   ngOnInit() {
-    this.tourService.initialize([{
-      anchorId: 'step3',
-      content: 'This is step three',
-      title: 'Step 3',
-    }, {
-      anchorId: 'step4',
-      content: 'This is step four',
-      title: 'Step 4',
-      enableBackdrop: true
-    }]);
+    this.tourService.initialize([
+      {
+        anchorId: 'step3',
+        content: 'This is step three',
+        title: 'Step 3',
+      }, {
+        anchorId: 'step4',
+        content: 'This is step four',
+        title: 'Step 4',
+        enableBackdrop: true
+      }]);
   }
 
   ngAfterViewInit() {
